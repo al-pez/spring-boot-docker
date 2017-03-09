0818d64d74f63a5c4795a563693b91cf6e227489
al-pez <alexperez.dev@gmail.com>
2017-03-09 21:48:51 +1300

    Refactor main directory and groupId

--- src/main/java/alpez/controller/IndexController.java
+++ src/main/java/alpez/controller/IndexController.java
@@ -1,4 +1,4 @@
-package mo.alpez.controller;
+package alpez.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
